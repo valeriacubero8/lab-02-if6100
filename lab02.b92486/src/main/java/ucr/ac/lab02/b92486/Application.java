ca85e0932adc54132be1a0325b6f36c651d79c08
valeriacubero <carmen.cuberocordoba@ucr.ac.cr>
2023-09-12 13:15:45 -0600

    Avance 1, configuracion y creacion de tablas

---
+++ lab02.b92486/src/main/java/ucr/ac/lab02/b92486/Application.java
@@ -0,0 +1,13 @@
+package ucr.ac.lab02.b92486;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+}
