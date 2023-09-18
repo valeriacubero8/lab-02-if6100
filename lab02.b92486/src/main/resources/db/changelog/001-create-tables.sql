323e6c36a2f187d30bde0e1209cde6ca54a06d76
valeriacubero <carmen.cuberocordoba@ucr.ac.cr>
2023-09-18 10:13:06 -0600

    Laboratorio terminado

--- lab02.b92486/src/main/resources/db/changelog/001-create-tables.sql
+++ lab02.b92486/src/main/resources/db/changelog/001-create-tables.sql
@@ -1,4 +1,3 @@
-
 CREATE TABLE b92486_rooms
 (
     id         UUID PRIMARY KEY,
@@ -11,7 +10,7 @@ CREATE TABLE b92486_users
     id       UUID PRIMARY KEY,
     user_Name VARCHAR(255) NOT NULL UNIQUE,
     room_Id  UUID,
-    FOREIGN KEY (room_Id) REFERENCES rooms (id)
+    FOREIGN KEY (room_Id) REFERENCES b92486_rooms (id)
 );
 
 CREATE TABLE b92486_messages
@@ -21,5 +20,5 @@ CREATE TABLE b92486_messages
     user_Name   VARCHAR(255),
     message    VARCHAR(1000),
     created_On DATE,
-    FOREIGN KEY (room_Id) REFERENCES rooms (id)
+    FOREIGN KEY (room_Id) REFERENCES b92486_rooms (id)
 );
